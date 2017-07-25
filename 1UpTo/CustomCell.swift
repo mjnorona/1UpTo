e3abb9e96715eb6a47aaba903f8f0bb0482a7c56
Marcus Norona <marcusjeremynorona@gmail.com>
2017-07-24 18:50:51 -0700

    added calendar cells

---
+++ 1UpTo/CustomCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomCell.swift
+//  1UpTo
+//
+//  Created by MJ Norona on 7/24/17.
+//  Copyright © 2017 MJ Norona. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CustomCell: JTAppleCell {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- 1UpTo/HomeViewController.swift
+++ 1UpTo/HomeViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import JTAppleCalendar
 
 class HomeViewController: UIViewController {
-
+    let formatter = DateFormatter()
     @IBOutlet weak var welcomeLabel: UILabel!
     @IBAction func signOutButtonPressed(_ sender: Any) {
         GIDSignIn.sharedInstance().signOut()
@@ -40,4 +41,24 @@ class HomeViewController: UIViewController {
     }
     */
 
+}
+
+extension HomeViewController: JTAppleCalendarViewDelegate, JTAppleCalendarViewDataSource {
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        formatter.dateFormat = "yyyy MM dd"
+        formatter.timeZone = Calendar.current.timeZone
+        formatter.locale = Calendar.current.locale
+        
+        let startDate = formatter.date(from: "2017 01 01")!
+        let endDate = formatter.date(from: "2017 12 31")!
+        
+        let parameters = ConfigurationParameters(startDate: startDate, endDate: endDate)
+        return parameters
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CustomCell", for: indexPath) as! CustomCell
+        cell.dateLabel.text = cellState.text
+        return cell
+    }
 }
